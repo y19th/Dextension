b95d7dc54ee39b61aab5f94e81b8b86ff05559e3
y19th-mac <whenmistdisappear@gmail.com>
2025-09-13 15:24:07 +0300

    refactoring

--- dextension-compose/src/commonMain/kotlin/com/y19th/dextension/compose/ScreenComponentExtension.kt
+++ dextension-compose/src/commonMain/kotlin/com/y19th/dextension/compose/ScreenComponentExtension.kt
@@ -3,7 +3,6 @@ package com.y19th.dextension.compose
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import com.y19th.dextension.core.BaseEvents
-import com.y19th.dextension.core.BaseState
 import com.y19th.dextension.core.ScreenComponent
 
 /**
